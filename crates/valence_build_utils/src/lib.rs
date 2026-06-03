741bf7c1046ce01ad6108b7efab7bbcb9f782aed
brittonr <b@robitzs.ch>
2026-06-03 19:01:06 -0400

    add valence_build_utils to strict Octet burndown

--- crates/valence_build_utils/src/lib.rs
+++ crates/valence_build_utils/src/lib.rs
@@ -1,42 +1,47 @@
 #![doc = include_str!("../README.md")]
 
-use std::path::Path;
-use std::process::Command;
-use std::{env, fs};
-
 use anyhow::Context;
-use proc_macro2::{Ident, Span, TokenStream};
 
-pub fn write_generated_file(content: TokenStream, out_file: &str) -> anyhow::Result<()> {
-    let out_dir = env::var_os("OUT_DIR").context("failed to get OUT_DIR env var")?;
-    let path = Path::new(&out_dir).join(out_file);
+pub fn write_generated_file(
+    content: proc_macro2::TokenStream,
+    out_file: &str,
+) -> anyhow::Result<()> {
+    let out_dir = std::env::var_os("OUT_DIR").context("failed to get OUT_DIR env var")?;
+    let path = std::path::Path::new(&out_dir).join(out_file);
     let code = content.to_string();
 
-    fs::write(&path, code)?;
+    std::fs::write(&path, code)?;
 
     // Try to format the output for debugging purposes.
     // Doesn't matter if rustfmt is unavailable.
-    let _ = Command::new("rustfmt").arg(path).output();
+    if std::process::Command::new("rustfmt")
+        .arg(path)
+        .output()
+        .is_err()
+    {
+        return Ok(());
+    }
 
     Ok(())
 }
 
 /// Parses a [`proc_macro2::Ident`] from a `str`. Rust keywords are prepended
 /// with underscores to make them valid identifiers.
-pub fn ident<I: AsRef<str>>(s: I) -> Ident {
+pub fn ident<I: AsRef<str>>(s: I) -> proc_macro2::Ident {
     let s = s.as_ref().trim();
 
     // Parse the ident from a str. If the string is a Rust keyword, stick an
     // underscore in front.
-    syn::parse_str::<Ident>(s)
-        .unwrap_or_else(|_| Ident::new(format!("_{s}").as_str(), Span::call_site()))
+    syn::parse_str::<proc_macro2::Ident>(s).unwrap_or_else(|_| {
+        proc_macro2::Ident::new(format!("_{s}").as_str(), proc_macro2::Span::call_site())
+    })
 }
 
 #[track_caller]
 pub fn rerun_if_changed<const N: usize>(files: [&str; N]) {
     for file in files {
         assert!(
-            Path::new(file).exists(),
+            std::path::Path::new(file).exists(),
             "File \"{file}\" does not exist. Did you forget to update the path?"
         );
 
